4657f0374aec9216330d240d934011609e8ceb13
upender0192 <uppysingh90@gmail.com>
2020-05-22 19:21:51 +0530

    new job

--- src/component/pages/newjob/newjob.js
+++ src/component/pages/newjob/newjob.js
@@ -1,9 +1,41 @@
 import React from 'react';
 
-const NewJob = () =>(
-    <div>
-        <h1>New Job</h1>
-    </div>
-)
+
+class NewJob extends React.Component{
+    constructor(){
+        super();
+
+        this.state = {
+                name:'',email:'',phone:'',profile:'' 
+            
+        }
+
+        //this.handelChange = this.handelChange.bind(this);
+        //this.handelSubmit = this.handelChange.bind(this);
+    }
+
+    handelChange = (e) => {
+        this.setState({[e.target.name] : e.target.value})
+    }
+
+    handelSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state)
+    }
+
+    render(){
+        return(
+            <div className="form-wrap">
+                <form onSubmit={this.handelSubmit}>
+                    <input type="text" placeholder="name" name="name" value={this.state.name} onChange={this.handelChange}></input>
+                    <input type="text" placeholder="email" name="email" value={this.state.email} onChange={this.handelChange}></input>
+                    <input type="text" placeholder="phone" name="phone" value={this.state.phone} onChange={this.handelChange}></input>
+                    <input type="text" placeholder="profile" name="profile" value={this.state.profile} onChange={this.handelChange}></input>
+                    <input type="submit" value="submit"></input>
+                </form>
+            </div>
+        )
+    }
+}
 
 export default NewJob;
